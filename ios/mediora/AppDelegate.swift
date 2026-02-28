abc58cfa991ccf5237579ba45398a57b489bec1a
ghobs91 <aghobrial@ptc.com>
2026-02-28 06:34:56 -0500

    fixed epg source

--- ios/mediora/AppDelegate.swift
+++ ios/mediora/AppDelegate.swift
@@ -31,7 +31,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     return true
   }
-}
 
   func applicationDidBecomeActive(_ application: UIApplication) {
     #if targetEnvironment(macCatalyst)
